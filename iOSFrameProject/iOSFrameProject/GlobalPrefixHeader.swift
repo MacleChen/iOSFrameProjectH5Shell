f384470f432e2f67778a79cbc66f3527fbf3b2fa
MacleChen <851327579@qq.com>
2018-03-12 15:51:24 +0800

    提交完成的H5壳子

---
+++ iOSFrameProject/iOSFrameProject/GlobalPrefixHeader.swift
@@ -0,0 +1,278 @@
+//
+//  GlobalPrefixHeader.swift
+//  EnvironmentSource
+//
+//  Created by jointsky on 2016/11/28.
+//  Copyright © 2016年 陈帆. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+
+/*******************   SCREEN  屏幕的尺寸     ******************/
+let SCREEN_WIDTH = UIScreen.main.bounds.size.width              // 宽
+let SCREEN_HEIGHT = UIScreen.main.bounds.size.height            // 高
+
+
+/********************    application Delegate  *************/
+let APP_DELEGATE = UIApplication.shared.delegate as! AppDelegate
+
+
+/*****************     VIEW 界面视图设置     *****************/
+// MARK:     建议：导航栏中的按钮图片的大小一般为26  26@2x   26@3x
+// 导航栏和状态栏的高度
+let NAVIGATION_AND_STATUS_HEIGHT: CGFloat = (44.0+20.0)
+ // 工具栏的高度
+let TOOL_BAR_HEIGHT: CGFloat = 49.0
+// tabBar和navBar上的图标的标准大小
+let ITEM_IMAGE_CGSZE = CGSize.init(width: 26, height: 26)
+// 状态栏的高度
+let STATUS_BAR_HEIGHT: CGFloat = 20.0
+// 导航栏的高度
+let NAVIGATION_BAR_HEIGHT: CGFloat = 44.0
+// 一般cell的高度
+let CELL_NORMAL_HEIGHT: CGFloat = 44.0
+// 每页显示的cell个数
+let DEFAULT_IMAGE_CELL_PAGESIZE = "19"
+
+
+/*********************   COLOR  - 颜色      **************************/
+// 系统普通颜色
+let COLOR_NORMAL_SYSTEM = UIColorRGBA_Selft(r: 255, g: 255, b: 255, a: 1.0)
+// 系统高亮颜色
+let COLOR_HIGHT_LIGHT_SYSTEM = UIColorRGBA_Selft(r: 252, g: 81, b: 81, a: 1.0)
+// 分割线的颜色
+let COLOR_SEPARATOR_LINE = UIColorFromRGB(rgbValue: 0xd9d9d9)
+// tableView 的背景色
+let BG_COLOR_TABLE_OR_COLLECTION = UIColorFromRGB(rgbValue: 0xf1f0f0)
+// light Gay color
+let COLOR_LIGHT_GAY = UIColorFromRGB(rgbValue: 0x999999)
+// Gay color
+let COLOR_GAY = UIColorFromRGB(rgbValue: 0x666666)
+// dark Gay color
+let COLOR_DARK_GAY = UIColorFromRGB(rgbValue: 0x333333)
+
+// carbon coin color
+let COLOR_CARBON_COIN = UIColorFromRGB(rgbValue: 0xe59c1e)
+
+
+/*********************    FONT  - 字体样式     **********************/
+//正常系统显示的字体
+let FONT_NORMAL_FAMILY = "Helvetica-Light"
+
+
+/*********************   FONTSIZE  - 字体大小     ********************/
+// MARK: 导航栏上的字体大小
+let NAVIGATION_TITLE_FONT_SIZE:CGFloat = 18.0
+// MARK: 大字体
+let FONT_BIG_SIZE:CGFloat = 16.0
+// MARK: 大字体
+let FONT_SYSTEM_SIZE:CGFloat = 15.0
+// MARK: 标准字体
+let FONT_STANDARD_SIZE:CGFloat = 14.0
+// mark: 小字体
+let FONT_SMART_SIZE:CGFloat = 12.0
+// mark: 非常小字体
+let FONT_VERY_SMART_SIZE:CGFloat = 9.0
+
+
+
+/********************     layout 布局设置    ****************/
+// 标准的圆角弧度
+let CORNER_NORMAL:CGFloat = 5.0
+// 标准的圆角弧度
+let CORNER_SMART:CGFloat = 3.0
+// 标准的圆角弧度
+let BORDER_WIDTH: CGFloat = 0.5
+
+
+/*******************    DATE 日期时间设置     *********************/
+// 日期的标准格式
+let DATE_STANDARD_FORMATTER = "yyyy-MM-dd HH:mm:ss"
+// 验证码重新获取描述（单位：s）
+let RUN_LOOP_VALUE = 60
+
+
+/************************     DICT_KEY 自定义         ********************/
+let DICT_TITLE = "title"             // 标题
+let DICT_SUB_TITLE = "subTitle"      // 标题
+let DICT_IMAGE_PATH = "imagePath"    // 本地图片地址
+let DICT_IDENTIFIER = "identifier"   // ID
+let DICT_IS_NEXT = "isNext"          // 是否有跳转
+let DICT_USER_INFO  = "userInfo"     // 用户信息字典key
+let DICT_SUB_VALUE1  = "value1"      // value1的key
+let DICT_SUB_VALUE2  = "value2"      // value2的key
+let DICT_SUB_VALUE3  = "value3"      // value3的key
+let DICT_IS_SHOW_COIN_TASK = "isShowCoinTask"   // 是否显示过碳币任务列表
+
+
+/*********************     LOCATION  定位设置     **********************/
+/// 默认 未定位成功的城市 和 code
+let DEFAULT_LOCATIONFAILED_CITY = "北京市"
+let DEFAULT_LOCATIONFAILED_CODE = "110000000"
+let DEFAULT_CITY_CENTER_LONGITUDE = "116.403406"
+let DEFAULT_CITY_CENTER_LATITUDE = "39.91582"
+/// 定位保存值
+let LOCATION_ADDRESS = "locationAddress"        // 定位地址
+let LOCATION_LATITUDE = "locationLatitude"      // 定位维度
+let LOCATION_LONGTITUDE = "locationLongitude"   // 定位经度
+
+
+/*****************     IMAGE_DEFAULT  默认图片     ********************/
+// 默认返回键的图片地址
+let DEFAULT_BACK_IMAGE_PATH = "default_back_icon.png"
+// 默认的用户头像
+let DEFAULT_USER_ICON = UIImage(named: "defaultUserImage.png")
+// 默认App图标
+let DEFUALT_APP_ICON = UIImage(named: "default_app_icon.png")
+
+
+
+/****************    AIR_QUALITY - 空气质量设置    **********************/
+let PHOTO_PM_25 = "pm25"                        // Pm2.5
+
+
+/****************  NOTIFICATION - 消息通知机制    *********************/
+let NOTIFICATION_UPDATE_UserInfo = "NotificationUpdateUserInfo"     // 用户消息通知
+let NOTIFICATION_UPDATE_PhotoInfo = "NotificationUpdatePhotoInfo"   // 图片消息通知
+let NOTIFICATION_UPDATE_UserOtherLogin = "NotificationUserOtherLogin"   // 用户其它地方登录通知
+let NOTIFICATION_UPDATE_UserRegister = "NotificationUserRegister"       // 用户注册成功通知
+let NOTIFICATION_UPDATE_CoinTaskUpdate = "NotificationCoinTaskupdate"   // 碳币任务更新
+
+
+/**************   ACCOUNT - 账户信息    ****************/
+let REQUEST_URL_INIT = "https://www.baidu.com"
+
+
+/*****************   CHARACTER 字符个数     ******************/
+let PHOTO_DESCRIPTION_LENGTH = 250 // 图片描述（字符）
+let SUGGEST_INFO_LENGTH = 250 //  意见反馈(字符）
+let WORDCOUNT_USERNAME = 20   // 用户昵称（字符）
+let WORDCOUNT_USER_SPEAK = 50 // 用户说说长度（字符）
+let WORDCOUNT_USER_PASSWORD = 20    // 用户密码长度（字符）
+let WORDCOUNT_USER_PASSWORD_MIN = 6 // 用户密码长度（字符）
+let WORDCOUNT_USER_PHONE = 11   // 手机号长度（字符）
+let WORDCOUNT_USER_EMAIL = 50   // 邮箱最大长度（字符）
+let WORDCOUNT_CHECK_CODE = 6    // 手机验证码长度（字符）
+
+
+/*****************    WORD_TIP 文字提示     ******************/
+let RESPONSE_DATA_NIL = "请求到的数据为空"
+let RESULT_WEB_ERROR = "网络异常，请稍后重试"
+let OTHER_LOGING_TIP = "该用户已在其它地方登录"
+let GLOBAL_USERINFO_NIL_TIP = "未获取到当前用户信息, 请重新登录"
+let GLOBAL_CHECK_PHONE_NOT_SEND_TIP = "该手机号未发送验证码"
+let GLOBAL_CHECK_PHONE_CHECK_CODE_TIP = "验证码不正确"
+let GLOBAL_CHECK_PASSWORD_LENGTH_TIP = "密码长度6~20个字符"
+
+
+/// 设置rgb颜色的方法
+///
+/// - parameter r: red
+/// - parameter g: green
+/// - parameter b: blue
+/// - parameter a: alpha
+///
+/// - returns: UIColor
+func UIColorRGBA_Selft(r:CGFloat, g:CGFloat, b:CGFloat, a:CGFloat) -> UIColor {
+    return UIColor(red: r/255.0, green: g/255.0, blue: b/255.0, alpha: a)
+}
+
+
+
+/// 根据开始位置和长度截取字符串
+///
+/// - Parameters:
+///   - textStr: 要截取的字符串
+///   - start: 开始位置
+///   - length: 截取长度
+/// - Returns: 截取后的字符串
+func CUTString(textStr: String, start:Int, length:Int = -1) -> String {
+    var len = length
+    if len == -1 {
+        len = textStr.count - start
+    }
+    let st = textStr.index(textStr.startIndex, offsetBy:start)
+    let en = textStr.index(st, offsetBy:len)
+    return String(textStr[st ..< en])
+}
+
+
+/// 16进制的方式设置颜色（eg. 0xff1122）
+///
+/// - Parameter rgbValue: 16进制颜色值
+/// - Returns: UIColor
+func UIColorFromRGB(rgbValue:Int) -> UIColor {
+    return UIColor(red: ((CGFloat)((rgbValue & 0xFF0000) >> 16))/255.0, green: ((CGFloat)((rgbValue & 0xFF00) >> 8))/255.0, blue: ((CGFloat)(rgbValue & 0xFF))/255.0, alpha: 1.0)
+}
+
+
+
+
+
+
+
+
+/// 自定义简单的打印
+///
+/// - parameter message: message
+func myPrint<T>(message: T) {
+    #if DEBUG
+        print("info:\(message)")
+    #endif
+}
+
+
+/// 获取默认的图片对象
+///
+/// - Returns: 默认占位图片
+func DEFAULT_IMAGE() -> UIImage {
+    let defaultImagePath = "default_image\((arc4random() % 5)+1)"
+    return UIImage.init(named: defaultImagePath)!
+}
+
+
+
+/// PM2.5值对应的颜色获取方法
+///
+/// - Parameter pm25Value: PM2.5值
+/// - Returns: UIColor
+func colorPm25WithValue(pm25Value: Int) -> UIColor {
+    if pm25Value >= 0 && pm25Value < 50 {
+        return UIColorFromRGB(rgbValue: 0x67cf00)
+    } else if pm25Value >= 50 && pm25Value < 100 {
+        return UIColorFromRGB(rgbValue: 0xfdd100)
+    } else if pm25Value >= 100 && pm25Value < 150 {
+        return UIColorFromRGB(rgbValue: 0xff8901)
+    } else if pm25Value >= 150 && pm25Value < 200 {
+        return UIColorFromRGB(rgbValue: 0xff2500)
+    } else if pm25Value >= 200 && pm25Value < 300 {
+        return UIColorFromRGB(rgbValue: 0x991753)
+    } else if pm25Value >= 300 && pm25Value < 500 {
+        return UIColorFromRGB(rgbValue: 0x62091d)
+    } else if pm25Value >= 500 {
+        return UIColor.black
+    } else {
+        return UIColor.white
+    }
+}
+
+
+
+/// 自定义带方法名和行号的打印方法
+///
+/// - parameter message:    message
+/// - parameter methodName: 方法名
+/// - parameter lineNumber: 行号
+func myPrint<T>(message: T, methodName: String = #function, lineNumber: Int = #line) {
+    #if DEBUG
+        print("\(methodName)[\(lineNumber)]:\(message)")
+    #endif
+}
+
+
+
+
+
+
---
+++ iOSFrameProject/iOSFrameProject/WKWebPageViewController.swift
@@ -0,0 +1,298 @@
+//
+//  WebPageViewController.swift
+//  ECOCityProject
+//
+//  Created by jointsky on 2017/10/9.
+//  Copyright © 2017年 陈帆. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class WKWebPageViewController: UIViewController, WKUIDelegate, WKNavigationDelegate, WKScriptMessageHandler {
+    
+    
+    var localPageUrlFilePath: String?   // 本地html的路径地址
+    var pageUrlStr: String?         // 页面URL
+    var pageThumbImageUrl: String?  // 分享宣传图片URL
+    var isUserGesture: Bool?        // 是否具有捏合收拾
+    var isShowShareBtn: Bool?       // 是否显示分享
+    
+    var isYearReportShare: Bool?    // 是否是年报分享
+    
+    var isAdaptNavigationHeight: Bool?  // 是否适配导航栏高度影响
+    
+    /// webView
+    fileprivate lazy var webView: WKWebView  = {
+        // 创建webveiew
+        // 创建一个webiview的配置项
+        let configuretion = WKWebViewConfiguration()
+        
+        // Webview的偏好设置
+        configuretion.preferences = WKPreferences()
+        
+        // ************   解决不能加载微信公众号文章在iOS11.0设备上的问题  ************ //
+        configuretion.preferences.minimumFontSize = 0
+        
+        configuretion.preferences.javaScriptEnabled = true
+        
+        // 默认是不能通过JS自动打开窗口的，必须通过用户交互才能打开
+        configuretion.preferences.javaScriptCanOpenWindowsAutomatically = false
+        
+        // 通过js与webview内容交互配置
+        configuretion.userContentController = WKUserContentController()
+        
+        // 添加一个名称，就可以在JS通过这个名称发送消息：
+        // window.webkit.messageHandlers.iOS.postMessage({body: 'xxx'})
+        configuretion.userContentController.add(self, name: "iOS")
+        
+        let webViewTemp = WKWebView.init(frame: CGRect.init(x: 0, y: STATUS_BAR_HEIGHT, width: SCREEN_WIDTH, height: SCREEN_HEIGHT-STATUS_BAR_HEIGHT), configuration: configuretion)
+        
+        webViewTemp.uiDelegate = self
+        webViewTemp.navigationDelegate = self
+        
+        // 是否适配导航栏高度影响
+        if self.isAdaptNavigationHeight != nil && self.isAdaptNavigationHeight! {
+            webViewTemp.frame.size.height = SCREEN_HEIGHT - NAVIGATION_AND_STATUS_HEIGHT
+        }
+        
+        // 去掉底部黑条
+        webViewTemp.isOpaque = false
+        webViewTemp.backgroundColor = UIColor.clear
+        
+        // 是否添加捏合收拾
+        if self.isUserGesture != nil && self.isUserGesture! {
+            webViewTemp.autoresizingMask = (UIViewAutoresizing(rawValue: UIViewAutoresizing.RawValue(UInt8(UIViewAutoresizing.flexibleWidth.rawValue) | UInt8(UIViewAutoresizing.flexibleHeight.rawValue))))
+            webViewTemp.allowsBackForwardNavigationGestures = true
+            webViewTemp.isMultipleTouchEnabled = true
+            //内容自适应
+            webViewTemp.sizeToFit();
+            webViewTemp.isUserInteractionEnabled = true
+        }
+        
+        self.view.addSubview(webViewTemp)
+        return webViewTemp
+    }()
+    
+    
+    // MARK: activityView
+    fileprivate lazy var activityView: UIActivityIndicatorView = {
+        let activityViewTemp = UIActivityIndicatorView.init(activityIndicatorStyle: UIActivityIndicatorViewStyle.gray)
+        
+        activityViewTemp.center = self.view.center
+        activityViewTemp.isHidden = true
+        
+        return activityViewTemp
+    }()
+    
+    
+    // MARK: ProgressView
+    fileprivate lazy var progressView: UIProgressView = {
+        let progressViewTemp = UIProgressView.init(frame: CGRect(x: 0, y: 0, width: SCREEN_WIDTH, height: 3))
+        progressViewTemp.trackTintColor = UIColor.clear
+        progressViewTemp.progressTintColor = UIColorFromRGB(rgbValue: 0x52afad)
+        progressViewTemp.transform = CGAffineTransform(scaleX: 1.0, y: 1.5)
+        
+        return progressViewTemp
+    }()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        
+        
+        self.setViewUI()
+    }
+    
+    // MARK: leftBarBtnItem Click
+    func leftBarBtnItemClick(sender: UIBarButtonItem) {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    // MARK: rightBarBtnItem Click
+    func rightBarBtnItemClick(sender: UIBarButtonItem) {
+        // 第三方分享
+        // 分享
+    }
+    
+    
+    // MARK: - UIWebViewDelegate 方法的实现
+    // MARK: did start load
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        //开始加载网页时展示出progressView
+        self.progressView.isHidden = false
+        //开始加载网页的时候将progressView的Height恢复为1.5倍
+        self.progressView.transform = CGAffineTransform(scaleX: 1.0, y: 1.5)
+        //防止progressView被网页挡住
+        self.view.bringSubview(toFront: self.progressView)
+    }
+    
+    // MARK: did finish load
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        //        self.webProgressLayer.finishedLoadWithError(nil)
+        if self.title == nil {
+            self.title = self.webView.title
+        }
+        
+        self.activityView.isHidden = true
+        self.activityView.stopAnimating()
+        
+        
+        //加载完成后隐藏progressView
+        //self.progressView.hidden = YES;
+        
+        // 检验是否是否返回（pop/back）
+        self.checkGoBack()
+        
+        // 禁止放大缩小
+        if self.isUserGesture == nil || !self.isUserGesture! {
+            let injectionJSString = "var script = document.createElement('meta');"
+                + "script.name = 'viewport';"
+                + "script.content=\"width=device-width, initial-scale=1.0,maximum-scale=1.0, minimum-scale=1.0, user-scalable=no\";"
+                + "document.getElementsByTagName('head')[0].appendChild(script);";
+            webView.evaluateJavaScript(injectionJSString, completionHandler: nil)
+        }
+    }
+    
+    
+    // MARK:  did finish error
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        self.activityView.isHidden = true
+        self.activityView.stopAnimating()
+        //        self.webProgressLayer.finishedLoadWithError(error)
+        
+        //加载失败同样需要隐藏progressView
+        //self.progressView.hidden = YES;
+        
+        // 检验是否是否返回（pop/back）
+        self.checkGoBack()
+    }
+    
+    // MARK:didReceive 
+    func webView(_ webView: WKWebView, didReceive challenge: URLAuthenticationChallenge,
+                 completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
+        let cred = URLCredential.init(trust: challenge.protectionSpace.serverTrust!)
+        completionHandler(.useCredential, cred)
+    }
+    
+    
+    // MARK: 服务器请求跳转的时候调用
+    func webView(_ webView: UIWebView, shouldStartLoadWith request: URLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+        
+        return true
+    }
+    
+    // MARK: js响应回调
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        myPrint(message: "body =\(message.body), name=\(message.name)")
+        let bodyStr = message.body as! String
+        if (bodyStr == "SystemDeviceInformation") {
+            let alert = UIAlertController.init(title: "提示", message: "请求：SystemDeviceInformation", preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction.init(title: "取消", style: UIAlertActionStyle.cancel, handler: nil))
+            
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // 析构方法 （回收垃圾）
+    deinit {
+        self.webView.removeObserver(self, forKeyPath: "estimatedProgress")
+    }
+    
+    
+    // MARK: view will disappear
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+    
+}
+
+extension WKWebPageViewController {
+    func setViewUI() {
+        // 设置 webView
+        let request: URLRequest?
+        self.pageUrlStr = REQUEST_URL_INIT
+//        self.localPageUrlFilePath = Bundle.main.path(forResource: "testButton.html", ofType: nil)
+        if self.localPageUrlFilePath == nil {
+            // 加载网页
+            request = URLRequest.init(url: URL.init(string: self.pageUrlStr!)!)
+            
+        } else {
+            // 加载本地文件
+            request = URLRequest.init(url: URL.init(fileURLWithPath: self.localPageUrlFilePath!))
+        }
+        
+        self.webView.load(request!)
+        
+        
+        // 设置加载圈
+        self.view.addSubview(self.activityView)
+        self.activityView.isHidden = false
+        self.activityView.startAnimating()
+        
+        
+        // 添加加载进度条
+        self.view.addSubview(self.progressView)
+        // 添加KVO进度加载方法
+        self.webView.addObserver(self, forKeyPath: "estimatedProgress", options: .new, context: nil)
+    }
+    
+    
+    @objc func goBack(){
+        self.webView.goBack()
+    }
+    
+    
+    /// 检查返回（pop/goback）
+    func checkGoBack(){
+        
+        self.navigationController?.interactivePopGestureRecognizer?.isEnabled = !self.webView.canGoBack
+        if self.webView.canGoBack{
+        }else{
+           
+        }
+    }
+    
+    
+    // MARK: 在监听方法中获取网页加载的进度，并将进度赋给progressView.progress
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "estimatedProgress" {
+            self.progressView.progress = Float(self.webView.estimatedProgress)
+            if self.progressView.progress == 1 {
+                /*
+                 *添加一个简单的动画，将progressView的Height变为1.4倍，在开始加载网页的代理中会恢复为1.5倍
+                 *动画时长0.25s，延时0.3s后开始动画
+                 *动画结束后将progressView隐藏
+                 */
+                UIView.animate(withDuration: 0.25, delay: 0.3, options: .curveEaseOut, animations: {
+                    self.progressView.transform = CGAffineTransform(scaleX: 1.0, y: 1.4)
+                }, completion: { (isFinish) in
+                    self.progressView.isHidden = true
+                })
+            }
+        } else {
+            super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
+        }
+    }
+}
+
+
