151a62b766f37f4190a1e6345d796455b127aa95
MacleChen <851327579@qq.com>
2018-03-22 18:39:26 +0800

    清空没有用的文件

--- iOSFrameProject/iOSFrameProject/AppDelegate.swift
+++ iOSFrameProject/iOSFrameProject/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        UIApplication.shared.statusBarStyle = UIStatusBarStyle.lightContent
+        UIApplication.shared.statusBarStyle = UIStatusBarStyle.default
         let functionStartsLocalize: FunctionStartsLocalize = FunctionStartsLocalize.init()
         let info = functionStartsLocalize.settingGDDTKey()
         
